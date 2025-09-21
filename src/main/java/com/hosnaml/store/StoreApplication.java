f95b1a1d310ddcf00cd44bc81f1fa6829f53ec46
hosna <hosna@kth.se>
2025-09-21 21:19:41 +0200

    Spring Boot project generated via start.spring.io

--- src/main/java/com/hosnaml/store/StoreApplication.java
+++ src/main/java/com/hosnaml/store/StoreApplication.java
@@ -1,4 +1,4 @@
-package com.Hosna.store;
+package com.hosnaml.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StoreApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StoreApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StoreApplication.class, args);
+    }
 
 }
